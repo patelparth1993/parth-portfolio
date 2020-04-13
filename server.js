cbf3025755f35f906c4669d7caa0315838b01bb8
patelparth1993 <patelparth3555@gmail.com>
2020-04-13 11:05:30 -0400

    v1.0.0

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const publicPath = path.join(__dirname, "build");
 app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
