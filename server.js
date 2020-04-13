1f33f6809c62bd5d17fcd916d6c697848acf8c1c
patelparth1993 <patelparth3555@gmail.com>
2020-04-13 10:45:40 -0400

    v1.0.0

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const publicPath = path.join(__dirname, "public");
+const publicPath = path.join(__dirname, "build");
 app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
